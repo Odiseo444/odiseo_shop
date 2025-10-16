1a92b7e881b1671241630cbba3ad2371a653482c
M4nu3lGit56 <manuelhmhd@gmail.com>
2025-10-16 11:51:29 -0500

    update

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $hacerConsulta = mysqli_query($conexion, $sql); ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Odiseo Shop | Moda Masculina</title>
+  <link href="/img/IMG-20250801-WA0005.jpg" rel="icon">
   <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
